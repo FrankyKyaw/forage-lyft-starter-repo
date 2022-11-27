8804df4f45d08747d0e5dab7cead82927e492cb3
Franky <myothetkyaw@bennington.edu>
2022-11-27 13:33:32 -0500

    fixed carfactory model

--- model/__init__.py
+++ model/__init__.py
@@ -1,12 +1,40 @@
 from abc import ABC, abstractmethod
-from engine.__init__ import Engine
-from battery.__init__ import Battery
+from battery.nubbin_battery import NubbinBattery
+from battery.spindler_battery import SpindlerBattery
+from engine.capulet_engine import CapuletEngine
+from engine.willoughby_engine import WilloughbyEngine
+from engine.sternman_engine import SternmanEngine
+from car import Car
 
-class CarFactory(ABC):
+class CarFactory:
     """Factory that creates a combination of engine and battery"""
 
-    def get_engine(self) -> Engine:
-        """Returns a new engine instance."""
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage,last_service_mileage)
+        battery = SpindlerBattery(current_date,last_service_date)
+        car = Car(engine, battery)
+        return car
 
-    def get_battery(self) -> Battery:
-        """Returns a new battery instance."""
+    def create_glissade(current_date, last_service_date, warning_light_is_on):
+        engine = SternmanEngine(warning_light_is_on)
+        battery = SpindlerBattery(current_date,last_service_date)
+        car = Car(engine, battery)
+        return car
+
+    def create_palindrome(current_date, last_service_date, warning_light_is_on):
+        engine = SternmanEngine(warning_light_is_on)
+        battery = SpindlerBattery(current_date,last_service_date)
+        car = Car(engine, battery)
+        return car
+
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = WilloughbyEngine(current_mileage,last_service_mileage)
+        battery = NubbinBattery(current_date,last_service_date)
+        car = Car(engine, battery)
+        return car
+
+    def create_Thovex(current_date, last_service_date, current_mileage, last_service_mileage):
+        engine = CapuletEngine(current_mileage,last_service_mileage)
+        battery = SpindlerBattery(current_date,last_service_date)
+        car = Car(engine, battery)
+        return car
